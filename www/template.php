fc3adde8244a32f4e4eed0bedbf28f98272c8b60
Simon Gellis <simongellis@gmail.com>
2014-05-11 01:41:29 -0400

    The site now forces you to be logged in

--- www/template.php
+++ www/template.php
@@ -3,6 +3,8 @@ include_once "functions.php";
 function begin_page($title) {
 	session_start();
 	echo "<html><head><title>$title</title></head><body>";
+	if (!$_SESSION["current_user_id"] && $_SERVER['PHP_SELF'] != "/login.php" && $_SERVER['PHP_SELF'] != "/createprofile.php")
+		header("location:login.php");
 }
 
 function end_page() {

79346e69239a86cbefb5be2df41bd2644c7266c1
stephwu2 <sw1787@nyu.edu>
2014-05-13 15:19:04 -0400

    Template for logout/search

--- www/template.php
+++ www/template.php
@@ -24,4 +24,39 @@ function end_page() {
 	echo "</body></html>";
 }
 
+function logout_search() {
+	echo "<a href=\"logout.php\">Logout</a> <br><br><br>";
+	echo '<form id="searchform" action="search.php" method="POST">' .
+		'<label name="term">Search:</label>' .
+		'<input id="search" name="term" type="text" />' .
+		'<input id="type" name="type" type="hidden" />' .
+		'<input type="submit" value ="Search"/>' .
+	'</form>' .
+
+	'<script type="text/javascript">'.
+		'$.widget("custom.uberAutocomplete", $.ui.autocomplete, {'.
+			'_renderItem: function( ul, item ) {'.
+				'var renderString = (item.value != "")'.
+					'? item.label + " (" + item.value + ")"'.
+					': item.label;'.
+				'return $( "<li>" )'.
+					'.append( $( "<a>" ).text( renderString ) )'.
+					'.appendTo( ul );'.
+			'}'.
+		'})'.
+		'$("#search").uberAutocomplete({'.
+			'source: "search/all.php",'.
+			'response: function(event, ui) {'.
+				'ui.content.unshift({label: $("#search").val(), value: ""});'.
+			'},'.
+			'select: function(event, ui) {'.
+				'event.preventDefault();'.
+				'$("#search").val(ui.item.label);'.
+				'$("#type").val(ui.item.value);'.
+				'return false;'.
+			'},'.
+		'});'.
+	'</script>';
+}
+
 ?>
